b9b515bc0d0fa8c67d051c2297cd8f9d07826e2b
suarezluis <suarezluis@gmail.com>
2018-05-25 00:20:36 -0500

    fine details and css adjusted

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,18 +4,19 @@ var friendsData = [
 	  photo: "https://cdn.vox-cdn.com/thumbor/9G1p7Ib7JBtpSXV3n1SQbvtlvfY=/0x0:3000x1962/1200x800/filters:focal(1260x741:1740x1221)/cdn.vox-cdn.com/uploads/chorus_image/image/49243107/484242770.0.jpg",
 	  scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	},
-	{
-	  name: "Donald Trump",
-	  photo: "https://i.imgflip.com/213kt2.jpg",
-	  scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-	},
+	
   
 	{
 	  name: "Hillary Clinton",
 	  photo:
 		"http://static.businessinsider.com/image/57a4f7cadb5ce99d168b510c-750.jpg",
-	  scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-	}
+	  scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	},
+	{
+		name: "Donald Trump",
+		photo: "https://i.imgflip.com/213kt2.jpg",
+		scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	  }
   ];
   
   module.exports = friendsData;
